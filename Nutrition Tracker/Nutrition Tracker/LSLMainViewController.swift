62f18c021fe4e72e0f57ed36199e51203c6e9a3e
Michael Stoffer <mstoffer@michaelstoffer.com>
2020-02-23 12:58:44 -0500

    Updated Sign Up button to have border

--- Nutrition Tracker/Nutrition Tracker/LSLMainViewController.swift
+++ Nutrition Tracker/Nutrition Tracker/LSLMainViewController.swift
@@ -11,7 +11,13 @@ import KeychainSwift
 
 class LSLMainViewController: UIViewController {
 
+    @IBOutlet var signInButton: CustomButton!
+    @IBOutlet var signUpButton: CustomButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        signUpButton.layer.borderWidth = 1
+        signUpButton.layer.borderColor = UIColor(red: 0.424, green: 0.388, blue: 1, alpha: 1).cgColor
     }
 }
