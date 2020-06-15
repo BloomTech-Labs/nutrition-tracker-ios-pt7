f9f34a1db46a04b7b59e5c914f9a17b0d3492931
Dillon <dillonp.lambda@gmail.com>
2020-06-14 18:20:53 -0700

    Added a new view controller to enable searching for foods via a UPC by using a barcode. Imported Apple's vision framework which will provide the functionality for scanning barcodes.

---
+++ Nutrivurv/Nutrivurv/LSLBarcodeSearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LSLBarcodeSearchViewController.swift
+//  Nutrivurv
+//
+//  Created by Dillon on 6/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+import Vision
+
+class LSLBarcodeSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
