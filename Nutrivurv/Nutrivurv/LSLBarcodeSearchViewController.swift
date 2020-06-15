73bf7763bceda79473a97f3995cc9fd4eeb387f4
Dillon <dillonp.lambda@gmail.com>
2020-06-15 15:52:18 -0700

    Fixed bug where activity indicator wouldn't be visible while app is searching for a food item by UPC. Changed the color of bar code scanner view to match main app colors. Added more detailed comments for easier understanding of functionality and improved code readability.

--- Nutrivurv/Nutrivurv/LSLBarcodeSearchViewController.swift
+++ Nutrivurv/Nutrivurv/LSLBarcodeSearchViewController.swift
@@ -5,10 +5,13 @@
 //  Created by Dillon on 6/14/20.
 //  Copyright © 2020 Lambda School. All rights reserved.
 //
+
 import AVFoundation
 import UIKit
 
 class LSLBarcodeSearchViewController: UIViewController, AVCapturePhotoCaptureDelegate {
+    
+    // MARK: - IBOutlets and Properties
     
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
@@ -25,9 +28,13 @@ class LSLBarcodeSearchViewController: UIViewController, AVCapturePhotoCaptureDel
     var searchController: LSLSearchController?
     
     var permissionGranted: Bool = false
-
+    
+    
+    // MARK: - View Life Cycle Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         setUpActivityView()
         checkPermissions()
         setUpCameraLiveView()
@@ -57,12 +64,12 @@ class LSLBarcodeSearchViewController: UIViewController, AVCapturePhotoCaptureDel
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-           super.viewWillDisappear(animated)
-
-           if (captureSession?.isRunning == true) {
-               captureSession.stopRunning()
-           }
-       }
+        super.viewWillDisappear(animated)
+        
+        if (captureSession?.isRunning == true) {
+            captureSession.stopRunning()
+        }
+    }
     
     // MARK: - Views & UI Setup
     
@@ -70,11 +77,11 @@ class LSLBarcodeSearchViewController: UIViewController, AVCapturePhotoCaptureDel
         activityIndicator.hidesWhenStopped = true
         activityIndicator.backgroundColor = UIColor(white: 1.0, alpha: 0.7)
         activityIndicator.layer.cornerRadius = 4
-        view.bringSubviewToFront(activityIndicator)
     }
     
     private func startLoadingView() {
         self.activityIndicator.startAnimating()
+        view.bringSubviewToFront(activityIndicator)
     }
     
     private func stopLoadingView() {
@@ -83,7 +90,7 @@ class LSLBarcodeSearchViewController: UIViewController, AVCapturePhotoCaptureDel
     
     private func addBarcodeScannerFrameView() {
         barcodeScannerFrameView = UIView()
-        barcodeScannerFrameView.layer.borderColor = UIColor.green.cgColor
+        barcodeScannerFrameView.layer.borderColor = UIColor(displayP3Red: 0.0, green: 66.0/255.0, blue: 108.0/255.0, alpha: 1.0).cgColor
         barcodeScannerFrameView.layer.borderWidth = 2
         view.addSubview(barcodeScannerFrameView)
         view.bringSubviewToFront(barcodeScannerFrameView)
@@ -98,14 +105,14 @@ class LSLBarcodeSearchViewController: UIViewController, AVCapturePhotoCaptureDel
         label.minimumScaleFactor = 0.5
         label.text = "Please grant access to the camera in order to scan barcodes."
         label.sizeToFit()
-
+        
         let button = UIButton(frame: CGRect(x: 0, y: label.frame.height + 8, width: view.frame.width * 0.8, height: 35))
         button.layer.cornerRadius = 10
         button.setTitle("Grant Access", for: .normal)
-        button.backgroundColor = UIColor(displayP3Red: 0.0/255.0, green: 66.0/255.0, blue: 108.0/255.0, alpha: 1)
+        button.backgroundColor = UIColor(displayP3Red: 0.0, green: 66.0/255.0, blue: 108.0/255.0, alpha: 1)
         button.setTitleColor(.white, for: .normal)
         button.addTarget(self, action: #selector(openSettings), for: .touchUpInside)
-
+        
         notAuthorizedAlertContainerView = UIView(frame: CGRect(
             x: view.frame.width * 0.1,
             y: (view.frame.height - label.frame.height + 8 + button.frame.height) / 2,
@@ -128,8 +135,8 @@ class LSLBarcodeSearchViewController: UIViewController, AVCapturePhotoCaptureDel
         
         switch status {
         case .denied, .restricted:
-            displayNotAuthorizedAlert()
             self.permissionGranted = false
+            displayNotAuthorizedAlert()
         case .notDetermined:
             // Prompt user for access to camera
             self.permissionGranted = false
@@ -138,7 +145,6 @@ class LSLBarcodeSearchViewController: UIViewController, AVCapturePhotoCaptureDel
                     self.permissionGranted = true
                     return
                 }
-                
                 DispatchQueue.main.async {
                     self.displayNotAuthorizedAlert()
                 }
@@ -151,17 +157,18 @@ class LSLBarcodeSearchViewController: UIViewController, AVCapturePhotoCaptureDel
     }
     
     private func setUpCameraLiveView() {
+        // Ensure we have appropriate permissions before a capture session is established
         guard permissionGranted != false else {
             return
         }
         
+        // Initialize capture session with appropriate camera
         self.captureSession = AVCaptureSession()
         self.captureSession.sessionPreset = .hd1920x1080
         
         let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera],
                                                                       mediaType: .video,
                                                                       position: .back)
-        
         let devices = deviceDiscoverySession.devices
         for device in devices {
             if device.position == .back {
@@ -169,15 +176,14 @@ class LSLBarcodeSearchViewController: UIViewController, AVCapturePhotoCaptureDel
             }
         }
         
-        // Ensure the camera is a back camera on this particular iPhone
         guard let backCamera = backCamera else {
             createAndDisplayAlertController(title: "Camera error", message: "We couldn't find a camera to use on your device")
             return
         }
         
+        // Establish input stream with appropriate device & add video input to capture session
         let videoInput: AVCaptureDeviceInput
         
-        // Establish input stream
         do {
             videoInput = try AVCaptureDeviceInput(device: backCamera)
         } catch {
@@ -192,22 +198,25 @@ class LSLBarcodeSearchViewController: UIViewController, AVCapturePhotoCaptureDel
             return
         }
         
+        // Initialize metadata output device for detecting barcode objects automatically
         let metadataOutput = AVCaptureMetadataOutput()
         
-        // This metdata allows the output device to interpret barcodes automatically
+        // Using this metadata output device eliminates the need for capturing & processing an image
         if (captureSession.canAddOutput(metadataOutput)) {
-            // Using this output device eliminates the need for a camera shutter button/processing image
             captureSession.addOutput(metadataOutput)
             
-            // Specificies the type of data to interpret from outputdevice (i.e. types of barcodes)
+            // Object interpretation delegate must be on main queue
             metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            
+            // Specificies which data types to interpret from output device (i.e. types of barcodes)
+            // Only these types of meta data will be forwarded to the delegate (self)
             metadataOutput.metadataObjectTypes = [.ean8, .ean13, .pdf417]
         } else {
             createAndDisplayAlertController(title: "Camera output error", message: "An output could not be established for the camera.")
             return
         }
         
-        // Add a preview layer for session
+        // Initialize & add preview layer for capture sesssion to establish an on screen view
         self.cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         cameraPreviewLayer?.videoGravity = .resizeAspectFill
         cameraPreviewLayer?.connection?.videoOrientation = .portrait
@@ -219,6 +228,7 @@ class LSLBarcodeSearchViewController: UIViewController, AVCapturePhotoCaptureDel
         }
         self.view.layer.addSublayer(previewLayer)
         
+        // If we get here, it's safe to start the capture session
         self.captureSession.startRunning()
     }
     
@@ -254,15 +264,20 @@ class LSLBarcodeSearchViewController: UIViewController, AVCapturePhotoCaptureDel
     
     // MARK: - Search by UPC
     
+    // Uses the search controller object to initiate a search
     func searchForFoodByUPC(_ upc: String) {
         self.searchController?.searchForFoodItemWithUPC(searchTerm: upc) {
             DispatchQueue.main.async {
+                
+                // Update UI by removing scanner frame view and stop activity indicator
+                self.barcodeScannerFrameView.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
                 self.stopLoadingView()
                 if self.searchController?.foods.count == 0 {
-                    self.barcodeScannerFrameView.removeFromSuperview()
+                    
+                    // Specialized alert controller that calls captureSession.startRunning() in the completion block
                     self.createAndDisplayAlertControllerAndStartCaptureSession(title: "No foods found", message: "We couldn't find any food matching this barcode. Please try again or search for this item manually.")
                 } else {
-                    self.delegate?.gotResultForFoodFromUPC()
+                    self.barcodeSearchDelegate?.gotResultForFoodFromUPC()
                     self.dismiss(animated: true)
                 }
             }
@@ -270,24 +285,33 @@ class LSLBarcodeSearchViewController: UIViewController, AVCapturePhotoCaptureDel
     }
 }
 
-// This delegate method handles the functionality for when metadata objects (barcodes) are detected
+
+// MARK: - AVCapture Metadata Output Object Delegate
+
+// Will inititiate a search by UPC automatically when barcode object is detected
 extension LSLBarcodeSearchViewController: AVCaptureMetadataOutputObjectsDelegate {
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        
+        // Stop capture session to prevent multiple network calls prior to completion
         captureSession.stopRunning()
+        
+        // Displays activity indicator to user while a search is in progress
         startLoadingView()
+        
         if let metadataObject = metadataObjects.first {
+            
+            // Interpret size of barcode on screen and set the scanner view box view to match that size
             let barcodeObject = self.cameraPreviewLayer?.transformedMetadataObject(for: metadataObject)
             barcodeScannerFrameView.frame = barcodeObject!.bounds
             
+            // Convert metadata to a readable object and get string value to initiate a UPC search
             guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else {
                 return
             }
-            
             guard let stringValue = readableObject.stringValue else {
                 return
             }
             AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
-            print(stringValue)
             searchForFoodByUPC(stringValue)
         }
     }
