35f3c0ff68601edad32159a9ab602161b21381a7
Dillon <dillonp.lambda@gmail.com>
2020-06-23 19:53:39 -0700

    Updated completion handlers for more precise error handling. Added KeychainSwift and associated properties, initializer, and methods to save a users token data to their keychain.

--- Nutrivurv/Nutrivurv/LSLAuthController.swift
+++ Nutrivurv/Nutrivurv/LSLAuthController.swift
@@ -7,17 +7,74 @@
 //
 
 import Foundation
+import KeychainSwift
 
 class AuthController {
-    static let shared  = AuthController()
+    static let shared = AuthController()
     
-    private let baseURL = URL(string: "https://nutrivurv-be.herokuapp.com/auth")!
+    static let authKeychainToken = "authorizationToken"
+    let keychain = KeychainSwift()
     
-    func loginUser(email: String, pasword: String, completion: @escaping (Error?) -> Void) {
+    private let baseURL = URL(string: "https://nutrivurv-be.herokuapp.com/api/auth")!
+    
+    func loginUser(user: userREST, completion: @escaping (Result<Bool, NetworkError>) -> Void) {
         
     }
     
-    func registerUser(name: String, email: String, password: String, completion: @escaping (Error?) -> Void) {
+    func registerUser(user: userREST, completion: @escaping (Result<Bool, NetworkError>) -> Void) {
+        let registerURL = baseURL.appendingPathComponent("register")
+        var request = URLRequest(url: registerURL)
         
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = HTTPMethod.post.rawValue
+        
+        let encoder = JSONEncoder()
+        
+        do {
+            request.httpBody = try encoder.encode(user)
+        } catch {
+            print("Error encoding user data for registration")
+            completion(.failure(.noEncode))
+            return
+        }
+
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                print("Error registering user: \(error)")
+                DispatchQueue.main.async {
+                    completion(.failure(.otherError))
+                    return
+                }
+            }
+            
+            guard let data = data else {
+                print("Error getting data from server")
+                DispatchQueue.main.async {
+                    completion(.failure(.badData))
+                }
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            
+            do {
+                let authResponse = try decoder.decode(AuthResponse.self, from: data)
+                if let token = authResponse.token {
+                    self.keychain.set(token, forKey: AuthController.authKeychainToken)
+                } else {
+                    print("Error saving token in keychain")
+                }
+            } catch {
+                print("Error decoding registration response data")
+                DispatchQueue.main.async {
+                    completion(.failure(.noDecode))
+                }
+                return
+            }
+            
+            completion(.success(true))
+            
+        }.resume()
     }
+    
 }
