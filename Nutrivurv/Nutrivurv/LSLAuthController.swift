b2568aee6b72c1ba8bcfa68c65c323dab7d63305
Dillon <dillonp.lambda@gmail.com>
2020-06-23 19:05:25 -0700

    Added blueprint for login/registration functions in AuthController

--- Nutrivurv/Nutrivurv/LSLAuthController.swift
+++ Nutrivurv/Nutrivurv/LSLAuthController.swift
@@ -9,5 +9,15 @@
 import Foundation
 
 class AuthController {
+    static let shared  = AuthController()
     
+    private let baseURL = URL(string: "https://nutrivurv-be.herokuapp.com/auth")!
+    
+    func loginUser(email: String, pasword: String, completion: @escaping (Error?) -> Void) {
+        
+    }
+    
+    func registerUser(name: String, email: String, password: String, completion: @escaping (Error?) -> Void) {
+        
+    }
 }
