22da10b48cca4016f21a0d7cba9feda78f8d55ac
Dillon <dillonp.lambda@gmail.com>
2020-06-23 18:35:32 -0700

    Added an AuthController class that will handle login and sign up via the new REST api backend

---
+++ Nutrivurv/Nutrivurv/LSLAuthController.swift
@@ -0,0 +1,13 @@
+//
+//  LSLAuthController.swift
+//  Nutrivurv
+//
+//  Created by Dillon on 6/23/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class AuthController {
+    
+}
