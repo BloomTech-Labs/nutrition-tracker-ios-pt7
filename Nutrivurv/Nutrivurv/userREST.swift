a99aa70fc5ee04cfeb4ed0973133542c086c03d5
Dillon <dillonp.lambda@gmail.com>
2020-06-23 19:15:52 -0700

    Added a new user model for the new REST backend

---
+++ Nutrivurv/Nutrivurv/userREST.swift
@@ -0,0 +1,15 @@
+//
+//  userREST.swift
+//  Nutrivurv
+//
+//  Created by Dillon on 6/23/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct userREST {
+    let name: String?
+    let email: String
+    let password: String
+}
