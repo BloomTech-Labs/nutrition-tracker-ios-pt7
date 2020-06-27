e2bdb5909dc6e893ed1d0935e29b26f034578698
Dillon <dillonp.lambda@gmail.com>
2020-06-27 15:11:15 -0700

    Added a food log controller class that will serve as data source and controller for FoodLogTableView

---
+++ Nutrivurv/Nutrivurv/FoodLogController.swift
@@ -0,0 +1,13 @@
+//
+//  FoodLogController.swift
+//  Nutrivurv
+//
+//  Created by Dillon on 6/27/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class FoodLogController {
+    
+}
