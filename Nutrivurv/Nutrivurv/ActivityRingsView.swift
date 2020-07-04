cb038a3b8911a7bdcdcb6de9403c16f4e303bb36
Dillon <dillonp.lambda@gmail.com>
2020-07-04 14:55:58 -0700

    Added Activity Rings custom SwiftUI view to project for user dashboard

---
+++ Nutrivurv/Nutrivurv/ActivityRingsView.swift
@@ -0,0 +1,21 @@
+//
+//  ActivityRingsView.swift
+//  Nutrivurv
+//
+//  Created by Dillon on 7/4/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import SwiftUI
+
+struct ActivityRingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ActivityRingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ActivityRingsView()
+    }
+}
