1c5f3dc1c2f681a515318bf163879fb25672faab
Dillon <dillonp.lambda@gmail.com>
2020-07-15 19:53:25 -0700

    Add initial blueprint for activity rings and animation to be used on user dashboard

--- Nutrivurv/Nutrivurv/ActivityRingsView.swift
+++ Nutrivurv/Nutrivurv/ActivityRingsView.swift
@@ -9,8 +9,24 @@
 import SwiftUI
 
 struct ActivityRingsView: View {
+    @State private var showCaloriesStroke = false
+    @State private var showCarbsStroke = false
+    @State private var showProteinsStroke = false
+    @State private var showFatsStroke = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Circle()
+                .trim(from: showCaloriesStroke ? 0.1 : 0.99, to: 1)
+                .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+                .rotationEffect(.degrees(90))
+                .rotation3DEffect(Angle(degrees: 180), axis: (x: 1, y: 0, z: 0))
+                .frame(width: 100, height: 100)
+                .animation(.easeOut)
+                .onTapGesture {
+                    self.showCaloriesStroke.toggle()
+            }
+        }
     }
 }
 
