0c9bbdf5edd527ac6221e44dc45971e8f8c2fb01
Dillon <dillonp.lambda@gmail.com>
2020-07-23 15:47:20 -0700

    Fixed a bug preventing the shadow of daily vibe background to change based on when trait collections change, such as when switching between light and dark mode while app is running.

--- Nutrivurv/Nutrivurv/DashboardViewController.swift
+++ Nutrivurv/Nutrivurv/DashboardViewController.swift
@@ -23,6 +23,23 @@ class DashboardViewController: UIViewController {
     
     let userController = ProfileCreationController()
     
+    
+    // MARK: Custom Views
+    
+    lazy var dailyVibeBackgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(named: "daily-vibe")
+        view.layer.cornerRadius = 14.0
+        view.layer.shadowColor = UIColor(named: "daily-vibe-shadow")!.cgColor
+        view.layer.shadowOpacity = 0.8
+        view.layer.shadowOffset = .zero
+        view.layer.shadowRadius = 14.0
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    
     // MARK: - View Lifecycle Methods and Update Views
     
     override func viewDidLoad() {
@@ -46,7 +63,6 @@ class DashboardViewController: UIViewController {
                     self.dailyVibeQuoteLabel.text = "\"\(quote.content)\""
                     self.dailyVibeAuthorLabel.text = quote.author
                     self.setUpDailyVibeBackgroundView()
-                    self.dailyVibeStackView.isHidden = false
                 default:
                     break
                 }
@@ -89,18 +105,24 @@ class DashboardViewController: UIViewController {
     }
     
     private func setUpDailyVibeBackgroundView() {
-        let view = UIView()
-        view.backgroundColor = UIColor(named: "daily-vibe")
-        view.layer.cornerRadius = 14.0
-        view.layer.shadowColor = UIColor(named: "daily-vibe-shadow")!.cgColor
-        view.layer.shadowOpacity = 0.75
-        view.layer.shadowOffset = .zero
-        view.layer.shadowRadius = 14.0
-        
-        view.translatesAutoresizingMaskIntoConstraints = false
-        dailyVibeStackView.insertSubview(view, at: 0)
-        view.pin(to: dailyVibeStackView)
+        dailyVibeStackView.insertSubview(self.dailyVibeBackgroundView, at: 0)
+        self.dailyVibeBackgroundView.pin(to: dailyVibeStackView)
+        self.dailyVibeStackView.isHidden = false
     }
+    
+    // MARK: Update Subviews
+    
+    // Need to manually update shadow color when trait collections change, as cgColor is not dynamic like UIColor
+    private func updateDailyVibeShaodw() {
+        self.dailyVibeBackgroundView.layer.shadowColor = UIColor(named: "daily-vibe-shadow")!.cgColor
+    }
+    
+    // Called when trait collections are made, like switching between Light <-> Dark mode to enable changes
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        self.updateDailyVibeShaodw()
+    }
+    
     
     // MARK: - IBActions
     
