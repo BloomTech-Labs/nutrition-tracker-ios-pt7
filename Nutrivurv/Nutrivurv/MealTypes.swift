d55a5ef0cadb8112315d14ded67d051618835c12
Dillon <dillonp.lambda@gmail.com>
2020-07-29 21:48:02 -0700

    Added new MealType model

---
+++ Nutrivurv/Nutrivurv/MealTypes.swift
@@ -0,0 +1,18 @@
+//
+//  MealTypes.swift
+//  Nutrivurv
+//
+//  Created by Dillon on 7/29/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+struct MealTypes {
+    var breakfast: String
+    var lunch: String
+    var dinner: String
+    var snack: String
+    var water: String
+}
