c7d9d69b6781e9c4c60ed5a84804fc45d911902e
Dillon <dillonp.lambda@gmail.com>
2020-08-04 20:34:36 -0700

    Added a generic method to fetch and return HealthKit data for a specific HKSampleType.

--- Nutrivurv/Nutrivurv/HealthKitController.swift
+++ Nutrivurv/Nutrivurv/HealthKitController.swift
@@ -10,6 +10,7 @@ import Foundation
 import HealthKit
 
 class HealthKitController {
+    
     class func authorizeHealthKit(completion: @escaping (Bool, Error?) -> Void) {
         guard HKHealthStore.isHealthDataAvailable() else {
             completion(false, HealthKitError.dataNotAvailable)
@@ -31,9 +32,35 @@ class HealthKitController {
         
         let healthKitTypesToRead: Set<HKObjectType> = [activeEnergy, basalEnergy, energyConsumed, carbs, fat, protein, weight]
         
-        
         HKHealthStore().requestAuthorization(toShare: healthKitTypesToWrite, read: healthKitTypesToRead) { (success, error) in
             completion(success, error)
         }
+    }
+    
+    class func getMostRecentSample(for sampleType: HKSampleType, completion: @escaping (HKQuantitySample?, Error?) -> Swift.Void) {
+        
+        let mostRecentPredicate = HKQuery.predicateForSamples(withStart: Date.distantPast, end: Date(), options: .strictEndDate)
+        
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)
+        
+        let limit = 1
+        
+        let sampleQuery = HKSampleQuery(sampleType: sampleType,
+                                        predicate: mostRecentPredicate,
+                                        limit: limit,
+                                        sortDescriptors: [sortDescriptor]) { (query, samples, error) in
+            
+            DispatchQueue.main.async {
+                
+                guard let samples = samples, let mostRecentSample = samples.first as? HKQuantitySample else {
+                    completion(nil, error)
+                    return
+                }
+                
+                completion(mostRecentSample, nil)
+            }
+        }
+        
+        HKHealthStore().execute(sampleQuery)
     }
 }
