34bd374a7218d70c94f322eba05b46f1aa3311d4
Dillon <dillonp.lambda@gmail.com>
2020-08-04 17:12:39 -0700

    Added new progress switcher view so user can toggle between seeing current progress and progress after adding in the selected meal.

---
+++ Nutrivurv/Nutrivurv/ProgressSwitcherView.swift
@@ -0,0 +1,66 @@
+//
+//  ProgressSwitcherView.swift
+//  Nutrivurv
+//
+//  Created by Dillon on 8/4/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProgressSwitcherView: View {
+    @State var currentProgress: Bool = true
+    
+    var unselectedTextColor: Color = Color(UIColor(named: "unselected-progress-switcher")!)
+    
+    var body: some View {
+        VStack {
+            Text("Today's Progress")
+                .font(Font.custom("Catamaran-Bold", size: 14))
+                .padding(EdgeInsets(top: 16, leading: 0, bottom: 0, trailing: 0))
+                .frame(height: 23)
+            
+            ZStack {
+                RoundedRectangle(cornerRadius: 14.0, style: .continuous)
+                    .foregroundColor(Color(UIColor(named: "progress-switcher-bg")!))
+                    .frame(width: 193, height: 28)
+                    .onTapGesture {
+                        self.currentProgress.toggle()
+                }
+                
+                RoundedRectangle(cornerRadius: 14.0, style: .continuous)
+                    .foregroundColor(Color(UIColor(named: "nutrivurv-blue-new")!))
+                    .frame(width: 100, height: 28)
+                    .offset(x: currentProgress ? -47 : 47, y: 0)
+                    .animation(.interactiveSpring(response: 0.24, dampingFraction: 0.74, blendDuration: 0.02))
+                
+                HStack {
+                    Text("currently")
+                        .font(Font.custom("Gaoel", size: 10))
+                        .foregroundColor(currentProgress ? .white : unselectedTextColor)
+                        .multilineTextAlignment(.center)
+                        .lineLimit(1)
+                        .frame(width: 64)
+                    Spacer()
+                    Text("after this")
+                        .font(Font.custom("Gaoel", size: 10))
+                        .foregroundColor(currentProgress ? unselectedTextColor : .white)
+                        .multilineTextAlignment(.center)
+                        .lineLimit(1)
+                        .frame(width: 73)
+                }
+                .padding(EdgeInsets(top: 0, leading: 18, bottom: 0, trailing: 12))
+                
+            }
+            .frame(width: 193, height: 28)
+            .padding(EdgeInsets(top: 0, leading: 0, bottom: 14, trailing: 0))
+            
+        }.frame(width: 193, height: 60)
+    }
+}
+
+struct ProgressSwitcherView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressSwitcherView()
+    }
+}
