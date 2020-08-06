294975bf8c2d81f27726d6e88d070eea474c8bb0
Dillon <dillonp.lambda@gmail.com>
2020-08-05 23:08:09 -0700

    Added an image url property and a new Measurements model to FoodLogEntry to be used for saving to the database.

--- Nutrivurv/Nutrivurv/FoodLogEntry.swift
+++ Nutrivurv/Nutrivurv/FoodLogEntry.swift
@@ -14,12 +14,14 @@ struct FoodLogEntry: Codable {
     var foodID: String
     var measurementURI: String
     var measurementName: String
+    var allMeasurements: [Measurement]
     var foodName: String
     var quantity: Int
     var calories: Int
     var fat: String
     var carbs: String
     var protein: String
+    var imageURL: String
     
     enum CodingKeys: String, CodingKey {
         case date
@@ -27,11 +29,23 @@ struct FoodLogEntry: Codable {
         case foodID = "edamam_food_id"
         case measurementURI = "measurement_uri"
         case measurementName = "measurement_name"
+        case allMeasurements = "all_measurements"
         case foodName = "food_name"
         case quantity
         case calories = "calories_kcal"
         case fat = "fat_g"
         case carbs = "carbs_g"
         case protein = "protein_g"
+        case imageURL = "image_url"
+    }
+}
+
+struct Measurement: Codable {
+    var uri: String
+    var label: String
+    
+    enum CodingKeys: String, CodingKey {
+        case uri = "measurement_uri"
+        case label = "measurement_name"
     }
 }
