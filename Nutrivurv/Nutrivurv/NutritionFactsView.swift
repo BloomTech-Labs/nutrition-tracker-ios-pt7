8c440b713ee85c68c2efadbd1f4ec760dfc4b3bb
dillonp23 <dillonp.lambda@gmail.com>
2020-08-08 17:17:36 -0700

    Added a new view to display a secondary (slidable) card sheet for the nutrition facts

---
+++ Nutrivurv/Nutrivurv/NutritionFactsView.swift
@@ -0,0 +1,21 @@
+//
+//  NutritionFactsView.swift
+//  Nutrivurv
+//
+//  Created by Dillon P on 8/8/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import SwiftUI
+
+struct NutritionFactsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NutritionFactsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NutritionFactsView()
+    }
+}
