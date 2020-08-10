9097245bbb66f652427d089abcea121194261436
Dillon <dillonp.lambda@gmail.com>
2020-08-09 17:11:07 -0700

    Added a new hosting controller class to enable displaying the SwiftUI detail view. Added a button in navigation bar to navigate to detail view for testing purposes.

---
+++ Nutrivurv/Nutrivurv/SwiftUIDetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SwiftUIDetailViewController.swift
+//  Nutrivurv
+//
+//  Created by Dillon on 8/9/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class SwiftUIDetailViewController: UIHostingController<FoodDetailView> {
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder, rootView: FoodDetailView(dailyMacros: DailyMacros()))
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .clear
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
