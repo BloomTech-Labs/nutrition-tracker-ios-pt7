b71093d8fb9efcd48cee6b2b0e83faecdf7fa372
Dillon <dillonp.lambda@gmail.com>
2020-08-11 18:50:42 -0700

    Added the HealthDashboardView to display HealthKit data to the user.

---
+++ Nutrivurv/Nutrivurv/HealthDashboardView.swift
@@ -0,0 +1,22 @@
+//
+//  HealthDashboardView.swift
+//  Nutrivurv
+//
+//  Created by Dillon on 8/11/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import SwiftUI
+import SwiftUICharts
+
+struct HealthDashboardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HealthDashboardView_Previews: PreviewProvider {
+    static var previews: some View {
+        HealthDashboardView()
+    }
+}
