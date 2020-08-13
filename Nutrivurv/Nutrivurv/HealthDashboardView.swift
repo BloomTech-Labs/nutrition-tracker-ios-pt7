4709f91f42c379c3d9f0f2a4b7be9ec3218b635b
Dillon <dillonp.lambda@gmail.com>
2020-08-12 19:44:01 -0700

    Added a view explaining what a caloric deficit is for user. Added padding and adjusted layout of view to ensure it is fully visible on all screen sizes.

--- Nutrivurv/Nutrivurv/HealthDashboardView.swift
+++ Nutrivurv/Nutrivurv/HealthDashboardView.swift
@@ -21,7 +21,7 @@ struct HealthDashboardView: View {
             Text("your health")
                 .font(Font.custom("Gaoel", size: 13))
                 .foregroundColor(Color(UIColor(named: "light-label")!))
-                .padding(.top, 60)
+                .padding(.top, UIScreen.main.bounds.height * 0.05)
             
             ScrollView(.vertical, showsIndicators: false) {
                 
@@ -43,7 +43,7 @@ struct HealthDashboardView: View {
                             VStack{
                                 HStack {
                                     Text("calories consumed")
-                                    .font(Font.custom("Gaoel", size: 14))
+                                        .font(Font.custom("Gaoel", size: 14))
                                         .padding(.leading, 6)
                                     
                                     Spacer()
@@ -66,30 +66,54 @@ struct HealthDashboardView: View {
                                     Text("today")
                                         .font(Font.custom("Gaoel", size: 12))
                                         .foregroundColor(Color.gray)
-                                    .padding(.leading, 6)
+                                        .padding(.leading, 6)
                                     
                                     Spacer()
                                     
                                     Text("2,579 cals")
                                         .font(Font.custom("Gaoel", size: 12))
                                         .foregroundColor(Color.gray)
-                                    .padding(.trailing, 6)
+                                        .padding(.trailing, 6)
                                     
                                 }.frame(width: 160, alignment: .leading)
-                                
-                                
                             }
                             .padding(.vertical, 16)
                             .padding(.horizontal, 10)
                         }
                     }
-                }.padding(.horizontal, 28)
-                    .padding(.top, 16)
+                    
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20, style: .continuous)
+                            .foregroundColor(Color(UIColor(named: "bg-color")!))
+                            .shadow(color: Color(UIColor(named: "daily-vibe-shadow")!), radius: 8.0, x: 0, y: 0)
+                        
+                        VStack {
+                            Text("what is a caloric deficit?")
+                                .foregroundColor(Color(UIColor(named: "light-label")!))
+                                .font(Font.custom("Gaoel", size: 14))
+                                .frame(alignment: .leading)
+                                .padding(.vertical, 6)
+                            
+                            Text("A caloric deficit is when the number of calories consumed is less than the number of total calories burned on any given day. The total calories burned is equal to the sum of both your active calories (calories burned during exercise) and your basal metabolic rate (BMR). Your BMR is the number of calories necessary for essential bodily functions, i.e. the base number of calories burned by your body every day even when you don't exercise. A caloric deficit is necessary in order to lose weight, and its currently accepted in the scientific community that one pound of fat is equivalent to roughly ~3,500 calories. This means that for every 3,500 calories in deficit, you'll lose approximately 1 pound.")
+                                .foregroundColor(Color.gray)
+                                .font(Font.custom("QuattrocentoSans-Bold", size: 13))
+                                .frame(alignment: .leading)
+                                .padding(.vertical, 6)
+                        }
+                        .frame(width: 320, alignment: .leading)
+                        
+                    }.frame(height:  260)
+                    
+                }
+                .padding(.horizontal, 28)
+                .padding(.top, 16)
+                .padding(.bottom, 80)
             }
             .background(Color(UIColor(named: "bg-color")!))
             
+            
         }.background(Color(UIColor(named: "bg-color")!))
-        .edgesIgnoringSafeArea(.all)
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
