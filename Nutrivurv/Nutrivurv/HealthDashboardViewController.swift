e141909d26e2b20eb36dbf08ee73219355cf5a26
Dillon <dillonp.lambda@gmail.com>
2020-08-14 11:06:25 -0700

    Refactor the active calories fetch method to work for all necessary calorie objects.

--- Nutrivurv/Nutrivurv/HealthDashboardViewController.swift
+++ Nutrivurv/Nutrivurv/HealthDashboardViewController.swift
@@ -12,6 +12,11 @@ import Combine
 import HealthKit
 
 class HealthDashboardViewController: UIHostingController<HealthDashboardView> {
+    var activeCalories: [(String, Int)]?
+    var basalCalories: [(String, Int)]?
+    var consumedCalories: [(String, Int)]?
+    
+    var caloricDeficits: [(String, Int)] = []
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder, rootView: HealthDashboardView(activeCalories: Calories(), caloricDeficit: Calories(), dailyMacros: FoodLogController.shared.totalDailyMacrosModel))
@@ -24,15 +29,21 @@ class HealthDashboardViewController: UIHostingController<HealthDashboardView> {
         self.navigationController?.navigationBar.backgroundColor = UIColor(named: "bg-color")!
         self.navigationController?.navigationBar.barTintColor = UIColor(named: "bg-color")!
         
-        getActiveCaloriesForWeek()
-    }
-    
-    private func getActiveCaloriesForWeek() {
-        guard let activeCalsSampleType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.activeEnergyBurned) else {
-            return
+        if let activeCalsBurned = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.activeEnergyBurned) {
+            self.getCalorieStatsCollectionForWeek(using: activeCalsBurned)
         }
         
-        HealthKitController.getCumulativeStatsCollectionUsingOneDayInterval(for: activeCalsSampleType) { (statsCollection, error) in
+        if let consumedCals = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.dietaryEnergyConsumed) {
+            self.getCalorieStatsCollectionForWeek(using: consumedCals)
+        }
+        
+        if let basalCalsBurned = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.basalEnergyBurned) {
+            self.getCalorieStatsCollectionForWeek(using: basalCalsBurned)
+        }
+    }
+    
+    private func getCalorieStatsCollectionForWeek(using quantityType: HKQuantityType) {
+        HealthKitController.getCumulativeStatsCollectionUsingOneDayInterval(for: quantityType) { (statsCollection, error) in
             if let error = error {
                 print(error)
                 return
@@ -67,30 +78,40 @@ class HealthDashboardViewController: UIHostingController<HealthDashboardView> {
                 }
             }
             
-            self.rootView.activeCalories.day1Label = activeCaloriesByDay[0].0
-            self.rootView.activeCalories.day1Count = activeCaloriesByDay[0].1
-            
-            self.rootView.activeCalories.day2Label = activeCaloriesByDay[1].0
-            self.rootView.activeCalories.day2Count = activeCaloriesByDay[1].1
-            
-            self.rootView.activeCalories.day3Label = activeCaloriesByDay[2].0
-            self.rootView.activeCalories.day3Count = activeCaloriesByDay[2].1
-            
-            self.rootView.activeCalories.day4Label = activeCaloriesByDay[3].0
-            self.rootView.activeCalories.day4Count = activeCaloriesByDay[3].1
-            
-            self.rootView.activeCalories.day5Label = activeCaloriesByDay[4].0
-            self.rootView.activeCalories.day5Count = activeCaloriesByDay[4].1
-            
-            self.rootView.activeCalories.day6Label = activeCaloriesByDay[5].0
-            self.rootView.activeCalories.day6Count = activeCaloriesByDay[5].1
-            
-            self.rootView.activeCalories.day7Label = activeCaloriesByDay[6].0
-            self.rootView.activeCalories.day7Count = activeCaloriesByDay[6].1
+            switch quantityType.identifier {
+            case HKQuantityTypeIdentifier.activeEnergyBurned.rawValue:
+                self.activeCalories = activeCaloriesByDay
+                
+                self.rootView.activeCalories.day1Label = activeCaloriesByDay[0].0
+                self.rootView.activeCalories.day1Count = activeCaloriesByDay[0].1
+                
+                self.rootView.activeCalories.day2Label = activeCaloriesByDay[1].0
+                self.rootView.activeCalories.day2Count = activeCaloriesByDay[1].1
+                
+                self.rootView.activeCalories.day3Label = activeCaloriesByDay[2].0
+                self.rootView.activeCalories.day3Count = activeCaloriesByDay[2].1
+                
+                self.rootView.activeCalories.day4Label = activeCaloriesByDay[3].0
+                self.rootView.activeCalories.day4Count = activeCaloriesByDay[3].1
+                
+                self.rootView.activeCalories.day5Label = activeCaloriesByDay[4].0
+                self.rootView.activeCalories.day5Count = activeCaloriesByDay[4].1
+                
+                self.rootView.activeCalories.day6Label = activeCaloriesByDay[5].0
+                self.rootView.activeCalories.day6Count = activeCaloriesByDay[5].1
+                
+                self.rootView.activeCalories.day7Label = activeCaloriesByDay[6].0
+                self.rootView.activeCalories.day7Count = activeCaloriesByDay[6].1
+                
+            case HKQuantityTypeIdentifier.basalEnergyBurned.rawValue:
+                self.basalCalories = activeCaloriesByDay
+            case HKQuantityTypeIdentifier.dietaryEnergyConsumed.rawValue:
+                self.consumedCalories = activeCaloriesByDay
+            default:
+                return
+            }
         }
     }
+    
 
-    private func getWeeklyCaloriesConsumed() {
-        
-    }
 }
