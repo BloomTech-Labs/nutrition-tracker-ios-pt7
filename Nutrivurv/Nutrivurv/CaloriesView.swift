2e194234ff2a692d3a018e9aaa81f11910724959
Dillon <dillonp.lambda@gmail.com>
2020-08-13 21:34:47 -0700

    Updated CaloriesView model to use the specific day of week label rather than hard coded weekday values.

--- Nutrivurv/Nutrivurv/CaloriesView.swift
+++ Nutrivurv/Nutrivurv/CaloriesView.swift
@@ -12,16 +12,15 @@ import SwiftUICharts
 struct CaloriesView: View {
     @ObservedObject var calories: Calories
     
-//    var data: ChartData = ChartData(values: [("Sunday", 523), ("Monday", 376), ("Tuesday", 298), ("Wednesday", 424), ("Thursday", 572), ("Friday", 0), ("Saturday", 0)])
     var title: String = "Calories"
-    var legend: String = "This Week"
+    var legend: String = "Last 7 Days"
     var style: ChartStyle = Styles.barChartStyleOrangeLight
     var form: CGSize = ChartForm.medium
     var image: Image = Image(systemName: "waveform.path.ecg")
     var valueSpecifier: String = "%.0f"
     
     var body: some View {
-        let caloriesData = ChartData(values: [("Sunday", calories.sundayCount), ("Monday", calories.mondayCount), ("Tuesday", calories.tuesdayCount), ("Wednesday", calories.wednesdayCount), ("Thursday", calories.thursdayCount), ("Friday", calories.fridayCount), ("Saturday", calories.saturdayCount)])
+        let caloriesData = ChartData(values: [(calories.day1Label, calories.day1Count), (calories.day2Label, calories.day2Count), (calories.day3Label, calories.day3Count), (calories.day4Label, calories.day4Count), (calories.day5Label, calories.day5Count), (calories.day6Label, calories.day6Count), (calories.day7Label, calories.day7Count)])
         
         return VStack {
             BarChartView(data: caloriesData, title: title, legend: legend, style: style, form: form, dropShadow: true, cornerImage: image, valueSpecifier: valueSpecifier)
