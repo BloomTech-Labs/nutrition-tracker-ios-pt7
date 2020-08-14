f60285ccced2c35a9f6e8dd4b64971dedf5221d8
Dillon <dillonp.lambda@gmail.com>
2020-08-13 21:45:11 -0700

    Added method to fetch active calories and get cumulative sums for each day of the week. Updated the root views active calories property with values in order to display the data to user on graph.

--- Nutrivurv/Nutrivurv/HealthDashboardViewController.swift
+++ Nutrivurv/Nutrivurv/HealthDashboardViewController.swift
@@ -23,18 +23,74 @@ class HealthDashboardViewController: UIHostingController<HealthDashboardView> {
         self.navigationController?.navigationBar.tintColor = UIColor(named: "bg-color")!
         self.navigationController?.navigationBar.backgroundColor = UIColor(named: "bg-color")!
         self.navigationController?.navigationBar.barTintColor = UIColor(named: "bg-color")!
-        // Do any additional setup after loading the view.
+        
+        getActiveCaloriesForWeek()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func getActiveCaloriesForWeek() {
+        guard let activeCalsSampleType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.activeEnergyBurned) else {
+            return
+        }
+        
+        HealthKitController.getCumulativeStatsCollectionUsingOneDayInterval(for: activeCalsSampleType) { (statsCollection, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            guard let statsCollection = statsCollection else {
+                print("error with stats collection data")
+                return
+            }
+            
+            let endDate = Date()
+            
+            guard let startDate = Calendar.current.date(byAdding: .day, value: -6, to: endDate) else {
+                print("error getting start date for statistics collection")
+                return
+            }
+            
+            var activeCaloriesByDay: [(String, Int)] = []
+            
+            statsCollection.enumerateStatistics(from: startDate, to: endDate) { (statistics, stop) in
+                if let caloriesSum = statistics.sumQuantity() {
+                    let date = statistics.startDate
+                    let weekDay = Calendar.current.component(.weekday, from: date)
+                    
+                    let dateFormatter = DateFormatter()
+                    let weekDayString = dateFormatter.weekdaySymbols[weekDay - 1]
+                    
+                    let calorieDouble = caloriesSum.doubleValue(for: HKUnit.kilocalorie())
+                    let calorieInt = Int(calorieDouble)
+                    
+                    activeCaloriesByDay.append((weekDayString, calorieInt))
+                }
+            }
+            
+            self.rootView.activeCalories.day1Label = activeCaloriesByDay[0].0
+            self.rootView.activeCalories.day1Count = activeCaloriesByDay[0].1
+            
+            self.rootView.activeCalories.day2Label = activeCaloriesByDay[1].0
+            self.rootView.activeCalories.day2Count = activeCaloriesByDay[1].1
+            
+            self.rootView.activeCalories.day3Label = activeCaloriesByDay[2].0
+            self.rootView.activeCalories.day3Count = activeCaloriesByDay[2].1
+            
+            self.rootView.activeCalories.day4Label = activeCaloriesByDay[3].0
+            self.rootView.activeCalories.day4Count = activeCaloriesByDay[3].1
+            
+            self.rootView.activeCalories.day5Label = activeCaloriesByDay[4].0
+            self.rootView.activeCalories.day5Count = activeCaloriesByDay[4].1
+            
+            self.rootView.activeCalories.day6Label = activeCaloriesByDay[5].0
+            self.rootView.activeCalories.day6Count = activeCaloriesByDay[5].1
+            
+            self.rootView.activeCalories.day7Label = activeCaloriesByDay[6].0
+            self.rootView.activeCalories.day7Count = activeCaloriesByDay[6].1
+        }
     }
-    */
 
+    private func getWeeklyCaloriesConsumed() {
+        
+    }
 }
