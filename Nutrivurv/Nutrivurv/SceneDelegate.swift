5504976a0b80ac6edd4ece636f27e8586829d38c
Dillon <dillonp.lambda@gmail.com>
2020-08-17 13:11:42 -0700

    Initialized HKControllerObservable class upon initial app load to speed up fetch times. Added a delay on launch screen to give HK time to load data before displaying app dashboard.

--- Nutrivurv/Nutrivurv/SceneDelegate.swift
+++ Nutrivurv/Nutrivurv/SceneDelegate.swift
@@ -12,30 +12,37 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
 
     var window: UIWindow?
-
+    let healthKitController = HealthKitControllerObservable()
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // This code runs upon app load to determine which view to present to user based on logged in state
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
             var rootVC: UIViewController?
             
-            if UserController.isLoggedIn() {
-                
-                // Custom class that returns a tab bar to be used throughout app
-                // This tab bar is the primary view controller for the Dashboard.storyboard
-                let tabBarVC = CustomTabBar.getTabBar()
-                
-                rootVC = tabBarVC
-            } else {
-                guard let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "MainAppWelcome") as? UINavigationController else {
-                    print("Unable to instantiate main")
-                    return
-                }
-                rootVC = vc
-            }
-            window.rootViewController = rootVC
-            self.window = window
+            window.rootViewController = UIStoryboard(name: "LaunchScreen", bundle: nil).instantiateInitialViewController()
             window.makeKeyAndVisible()
+            
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 3) {
+                
+                if UserController.isLoggedIn() {
+                    
+                    let tabBarVC = CustomTabBar.getTabBar()
+                    
+                    rootVC = tabBarVC
+                    // Custom class that returns a tab bar to be used throughout app
+                    // This tab bar is the primary view controller for the Dashboard.storyboard
+                } else {
+                    guard let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(identifier: "MainAppWelcome") as? UINavigationController else {
+                        print("Unable to instantiate main")
+                        return
+                    }
+                    rootVC = vc
+                }
+                window.rootViewController = rootVC
+                self.window = window
+                window.makeKeyAndVisible()
+            }
         }
     }
 
