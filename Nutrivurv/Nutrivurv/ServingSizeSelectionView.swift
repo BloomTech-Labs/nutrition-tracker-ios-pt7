d6fcaa5f3e59882c68608f47a38b190862a5e7b6
Dillon <dillonp.lambda@gmail.com>
2020-08-20 16:51:43 -0700

    Fixed a bug causing calorie data to fetch before being added to HK after logging a new meal

--- Nutrivurv/Nutrivurv/ServingSizeSelectionView.swift
+++ Nutrivurv/Nutrivurv/ServingSizeSelectionView.swift
@@ -9,35 +9,35 @@
 import SwiftUI
 
 struct ServingSizeSelectionView: View {
+    @Binding var selectedQuantity: Double
+    @Binding var selectedServingSize: String
+    @Binding var selectedMealType: String
+    
     var quantityLabel = "Quantity"
     var servingSizeLabel = "Serving Size"
     var mealTypeLabel = "Meal Type"
     
-    var selectedQuantity: String = "1.0"
-    var selectedServingSize: String = "Whole"
-    var selectedMealType: String = "Breakfast"
-    
     var body: some View {
         HStack {
-            FoodDetailsInputView(label: quantityLabel, input: selectedQuantity)
+            FoodDetailsInputViewDouble(label: quantityLabel, inputDouble: $selectedQuantity)
             Spacer()
-            FoodDetailsInputView(label: servingSizeLabel, input: selectedServingSize)
+            FoodDetailsInputViewString(label: servingSizeLabel, inputString: $selectedServingSize)
             Spacer()
-            FoodDetailsInputView(label: mealTypeLabel, input: selectedMealType)
+            FoodDetailsInputViewString(label: mealTypeLabel, inputString: $selectedMealType)
         }
     }
 }
 
 struct ServingSizeSelection_Previews: PreviewProvider {
     static var previews: some View {
-        ServingSizeSelectionView()
+        ServingSizeSelectionView(selectedQuantity: .constant(1.0), selectedServingSize: .constant("Whole"), selectedMealType: .constant("Breakfast"))
     }
 }
 
 
-struct FoodDetailsInputView: View {
-    @State var label: String
-    @State var input: String
+struct FoodDetailsInputViewString: View {
+    var label: String
+    @Binding var inputString: String
     
     var body: some View {
         VStack {
@@ -46,10 +46,33 @@ struct FoodDetailsInputView: View {
                 .frame(height: 13)
                 .padding(EdgeInsets(top: 10, leading: 0, bottom: 4, trailing: 0))
             
-            Text(input)
+            Text(inputString)
                 .font(Font.custom("Catamaran-SemiBold", size: 16))
                 .frame(width: 90, height: 31)
             
+            RoundedRectangle(cornerRadius: 1.0, style: .continuous)
+                .stroke(lineWidth: 1.0)
+                .foregroundColor(Color(UIColor(named: "nutrivurv-blue-new")!))
+                .frame(width: 58, height: 0.5).padding(EdgeInsets(top: -16, leading: 0, bottom: 0, trailing: 0))
+        }.frame(width: 100, height: 50)
+    }
+}
+
+struct FoodDetailsInputViewDouble: View {
+    var label: String
+    @Binding var inputDouble: Double
+
+    var body: some View {
+        VStack {
+            Text(label)
+                .font(Font.custom("QuattrocentoSans-Italic", size: 12))
+                .frame(height: 13)
+                .padding(EdgeInsets(top: 10, leading: 0, bottom: 4, trailing: 0))
+
+            Text("\(inputDouble)")
+                .font(Font.custom("Catamaran-SemiBold", size: 16))
+                .frame(width: 90, height: 31)
+
             RoundedRectangle(cornerRadius: 1.0, style: .continuous)
                 .stroke(lineWidth: 1.0)
                 .foregroundColor(Color(UIColor(named: "nutrivurv-blue-new")!))
