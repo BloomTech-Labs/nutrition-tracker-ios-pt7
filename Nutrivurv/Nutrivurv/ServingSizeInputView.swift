6bb5f0ff1927f5f99a5e9720d943d7cce8f313bc
Dillon <dillonp.lambda@gmail.com>
2020-08-23 02:23:50 -0700

    Implemented the custom picker into the ServingSizeInputView

--- Nutrivurv/Nutrivurv/ServingSizeInputView.swift
+++ Nutrivurv/Nutrivurv/ServingSizeInputView.swift
@@ -9,13 +9,52 @@
 import SwiftUI
 
 struct ServingSizeInputView: View {
+    @Binding var showServingSizes: Bool
+    @Binding var showQuantity: Bool
+    
+    @Binding var selectedServingSize: String
+    var servingSizes: [Measure]
+    
+    var screenWidth = UIScreen.main.bounds.width
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        BottomSheetModal(display: $showServingSizes) {
+            VStack {
+                ZStack {
+                    ServingSizePicker(selectedItem: self.$selectedServingSize, data: self.servingSizes)
+                    
+                    Text("What's the measurement?")
+                    .font(.custom("QuattrocentoSans-BoldItalic", size: 18))
+                    .foregroundColor(Color(UIColor(named: "light-label")!))
+                    .frame(height: 70)
+                    .background(Color.white.opacity(0.8))
+                    .offset(y: -55)
+                    
+                    Button(action: {
+                        // Dismiss the keyboard when navigating to next view
+                        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                        self.showServingSizes.toggle()
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.015) {
+                            self.showQuantity.toggle()
+                        }
+                    }) {
+                        Text("Serving Sizes >")
+                    }
+                    .frame(width: 180, height: 40)
+                    .background(Color.white.opacity(0.8))
+                    .offset(y: 110)
+                }
+                .frame(alignment: .top)
+                .offset(y: -45)
+            }
+        }
     }
 }
 
 struct ServingSizeInputView_Previews: PreviewProvider {
     static var previews: some View {
-        ServingSizeInputView()
+        let servingSizes = [Measure(uri: "1", label: "Serving"), Measure(uri: "2", label: "Whole"), Measure(uri: "3", label: "Cup"), Measure(uri: "4", label: "Ounce"), Measure(uri: "5", label: "Gram")]
+        
+        return ServingSizeInputView(showServingSizes: .constant(true), showQuantity: .constant(false), selectedServingSize: .constant("Serving"), servingSizes: servingSizes)
     }
 }
