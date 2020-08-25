0bcb5c9594d8ef5319c85d739c2b5ef4df1fcf6e
Dillon <dillonp.lambda@gmail.com>
2020-08-25 12:17:23 -0700

    Updated no health data view.

--- Nutrivurv/Nutrivurv/HKNotAuthorizedView.swift
+++ Nutrivurv/Nutrivurv/HKNotAuthorizedView.swift
@@ -15,14 +15,26 @@ struct HKNotAuthorizedView: View {
                 VStack {
                     Image("favor_1")
                         .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(Color.pink)
                         .frame(width: 80, height: 80, alignment: .center)
                         .padding(.bottom)
                     
-                    Text("We need permission to access you health data")
+                    Text("No Health Data")
+                        .font(Font.custom("QuattrocentoSans-Bold", size: 22))
+                        .multilineTextAlignment(.center)
+                        .frame(width: 180)
+                    
+                    Text("Grant Nutrivurv access to your health data in the settings app, then start logging your meals to track your proress!")
+                        .font(Font.custom("QuattrocentoSans", size: 16))
+                        .multilineTextAlignment(.center)
+                        .frame(width: 210)
+                        .padding(.top)
                 }
-                .frame(width: 200, height: 200)
+                .frame(width: 220, height: 240)
                 .padding()
                 .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 60, style: .continuous))
             }
             .background(Color.black.opacity(0.5))
         }.edgesIgnoringSafeArea(.all)
